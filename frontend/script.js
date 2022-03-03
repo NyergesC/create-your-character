c9b36cb53894bb6a5a3c214252a0ac27074e101f
NyergesC <cintia.nyerges@gmail.com>
2022-03-03 12:24:59 +0100

    input event

---
+++ frontend/script.js
@@ -0,0 +1,13 @@
+function loadEvent () {
+
+  const input = document.querySelector('input');
+  const log = document.getElementById('values');
+
+  input.addEventListener('input', updateValue);
+
+  function updateValue(e) {
+    log.textContent = e.target.value;
+  }
+}
+
+window.addEventListener("load", loadEvent)
